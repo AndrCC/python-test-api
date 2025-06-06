56063452b876e20c6a8b0db6bdf7521d8faeb600
Andre <andre.cavalcantte@hotmail.com>
2025-06-06 09:44:25 -0300

    feat: valida entrada para impedir letras e símbolos inválidos no CPF

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, jsonify
+import re
 
 app = Flask(__name__)
 
 with open("blacklist.txt", "r") as file:
-    blacklist = set(line.strip() for line in file if line.strip())
+    blacklist = set(
+        re.sub(r'\D', '', line.strip()) for line in file if line.strip()
+    )
 
 @app.route("/<cpf>")
 def check_cpf(cpf):
-    if cpf in blacklist:
-        return jsonify({"status": "BLOCK"})
-    else:
-        return jsonify({"status": "FREE"})
+    if not re.fullmatch(r"[\d\.\-]+", cpf):
+        return jsonify({"error": "CPF contém caracteres inválidos."}), 400
+
+    cpf = re.sub(r'\D', '', cpf)
+
+    if len(cpf) != 11:
+        return jsonify({"error": "CPF inválido. Deve conter exatamente 11 dígitos."}), 400
+
+    status = "BLOCK" if cpf in blacklist else "FREE"
+    return jsonify({"status": status})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
